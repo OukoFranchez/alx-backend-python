9edd483aec3886de6b075cbeec3ca8ed43e1c2c7
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-15 01:40:19 +0300

    add solution to task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def add(a: float, b:float) -> float:
+    """
+	Calculates the sum of two numbers.
+
+	Parameters:
+	a (float): The first number.
+	b (float): The second number.
+
+	Returns:
+	float: The sum of a and b.
+	"""
+    return a + b
