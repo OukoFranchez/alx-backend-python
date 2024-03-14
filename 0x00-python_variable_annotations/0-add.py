33f132f49ad70f73d930040eeab9f98dfc53bfaa
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-15 01:55:22 +0300

    correct indentation errors

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 def add(a: float, b:float) -> float:
     """
-	Calculates the sum of two numbers.
+    Calculates the sum of two numbers.
 
-	Parameters:
-	a (float): The first number.
-	b (float): The second number.
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
 
-	Returns:
-	float: The sum of a and b.
-	"""
+    Returns:
+    float: The sum of a and b.
+    """
     return a + b
+
