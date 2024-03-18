da9179bf79d9a8f689c4877a90c899b357e4df18
OukoFranchez <oukofranchez@gmail.com>
2024-03-18 20:08:03 +0300

    refactor task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,36 +2,16 @@
 """ Task 2 Module """
 
 
+import asyncio
 import time
-from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     """
-    This module contains a function called `measure_time` that measures
-the average time it takes to execute the `wait_n` function multiple times.
-
-Example Usage:
-    average_time = measure_time(5, 10)
-    print(average_time)
-
-In this example, the `measure_time` function is
-called with `n=5` and `max_delay=10`.
-It measures the average time it takes to execute the `wait_n`
-function 5 times with a maximum delay of 10.
-
-Inputs:
-    n (int): The number of times to execute the `wait_n` function.
-    max_delay (int): The maximum delay for
-    each execution of the `wait_n` function.
-
-Returns:
-    float: The average time it takes to execute
-    the `wait_n` function multiple times.
+    Calculates caroutine runtime
     """
-    start_time = time.time()
-    wait_n(n, max_delay)
-    end_time = time.time()
-    total_time = end_time - start_time
-    return total_time / n
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    return (end - start) / n
